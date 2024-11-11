9be74d8113f409f4500128df97dfff5b889ddf2f
Chamudi Upeka <129956853+chamudiupeka@users.noreply.github.com>
2024-11-11 06:03:11 +0530

    update side bar js and css of sales and marketing manager

---
+++ src/roles/salesManager/javaScript/sidebar.js
@@ -0,0 +1,57 @@
+const toggleButton = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+const subMenu = document.getElementById('sub-menu');
+const dropdownButtonImage = document.getElementById('dropdownbtn-img');
+const sidebarTitles = document.querySelectorAll('.sidebar-titles');
+const userTitle = document.querySelector('.user-title');
+
+function toggleSidebar() {
+  sidebar.classList.toggle('close');
+  toggleButton.classList.toggle('rotate');
+  sidebarTitles.forEach((title) => {
+    title.classList.toggle('hide');
+  });
+
+  userTitle.classList.toggle('hide');
+  dropdownButtonImage.classList.toggle('hide');
+  closeAllSubMenus();
+}
+
+
+function toggleSubMenu() {
+  if (sidebar.classList.contains('close')) {
+    sidebar.classList.remove('close');
+    toggleButton.classList.remove('rotate');
+
+    sidebarTitles.forEach((title) => {
+      title.classList.toggle('hide');
+    });
+    userTitle.classList.toggle('hide');
+    dropdownButtonImage.classList.add('hide');
+  }
+
+  dropdownButtonImage.classList.remove('hide');
+  subMenu.classList.toggle('show');
+  dropdownButtonImage.classList.toggle('rotate');
+}
+
+function closeAllSubMenus() {
+  Array.from(sidebar.getElementsByClassName('sub-menu')).forEach((ul) => {
+    ul.classList.remove('show');
+    ul.previousElementSibling.classList.remove('rotate');
+  });
+}
+
+// // Close all open submenus before toggling the current one
+//   if (!button.nextElementSibling.classList.contains('show')) {
+//     closeAllSubMenus();
+//   }
+
+//   button.nextElementSibling.classList.toggle('show');
+//   button.classList.toggle('rotate');
+
+//   // If the sidebar is in a closed state, open it
+//   if (sidebar.classList.contains('close')) {
+//     sidebar.classList.remove('close');
+//     toggleButton.classList.toggle('rotate');
+//   }
