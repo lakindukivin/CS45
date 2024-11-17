e35cee8983e9a481f77457f154260cf47b267b85
Chamudi Upeka <129956853+chamudiupeka@users.noreply.github.com>
2024-11-18 03:52:14 +0530

    Updated admin's Site Performance html,css,js

---
+++ src/roles/admin/javaScript/sitePerformance.js
@@ -0,0 +1,12 @@
+
+
+// Error Logs Modal Control
+function openErrorLogs() {
+  document.getElementById('errorLogsModal').style.display = 'block';
+  populateErrorLogs();
+}
+
+function closeErrorLogs() {
+  document.getElementById('errorLogsModal').style.display = 'none';
+}
+ 
