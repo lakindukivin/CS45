5b90b524241d86549489dafe98bf9057b31099bc
Chamudi Upeka <129956853+chamudiupeka@users.noreply.github.com>
2024-11-26 22:53:25 +0530

    Updated products html css js

---
+++ src/roles/salesManager/javaScript/modal.js
@@ -0,0 +1,17 @@
+//Modal Control
+function openAddModal() {
+  document.getElementById('addModal').style.display = 'block';
+}
+
+function closeAddModal() {
+  document.getElementById('addModal').style.display = 'none';
+}
+
+//edit Control
+function openEditModal() {
+  document.getElementById('editModal').style.display = 'block';
+}
+
+function closeEditModal() {
+  document.getElementById('editModal').style.display = 'none';
+}
