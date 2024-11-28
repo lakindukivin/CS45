8ddc922feb5ecec814d4c856d86c11f4fc4caaa6
Chamudi Upeka <129956853+chamudiupeka@users.noreply.github.com>
2024-11-28 20:37:54 +0530

    Updated product backend php

---
+++ app/models/database.php
@@ -0,0 +1,25 @@
+<?php
+
+class Database
+{
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $dbname = "waste360";
+    public $conn;
+
+    public function __construct()
+    {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->dbname);
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->conn;
+    }
+}
+
+?>
