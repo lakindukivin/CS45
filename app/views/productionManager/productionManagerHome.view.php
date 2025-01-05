c142986cbc8c9da7ae3777d3e0bce2fad85c3ad2
nimashav <nnimasha43@gmail.com>
2025-01-06 00:20:55 +0530

    do some changes of production manager pages

--- app/views/productionManager/productionManagerHome.view.php
+++ app/views/productionManager/productionManagerHome.view.php
@@ -4,49 +4,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= ROOT ?>/src/roles/productionManager/styles/sidebar.css">
-    <link rel="stylesheet" href="<?= ROOT ?>/src/roles/productionManager/styles/home.css">
-    <link rel="stylesheet" href="<?= ROOT ?>/src/roles/productionManager/styles/common.css">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/productionManager/sidebar.css" />
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/productionManager/home.css">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/productionManager/common.css">
     <title>Waste360|Dashboard|PM</title>
 </head>
 
 <body>
     <nav id="sidebar">
         <button id="toggle-btn" onclick="toggleSidebar()" class="toggle-btn">
-            <img src="<?= ROOT ?>/src/assets/menu.svg" alt="menu" />
+            <img src="<?= ROOT ?>/assets/images/menu.svg" alt="menu" />
         </button>
         <div class="sidebar-container">
             <div class="prof-picture">
-                <img src="<?= ROOT ?>/src/assets/user.svg" alt="profile" />
+                <img src="<?= ROOT ?>/assets/images/user.svg" alt="profile" />
                 <span class="user-title">Production Manager</span>
             </div>
 
             <div>
                 <ul>
                     <li>
-                        <a href="#" class="sidebar-active"><img src="<?= ROOT ?>/src/assets/dashboard.svg" alt="dashboard" /><span
+                        <a href="#" class="sidebar-active"><img src="<?= ROOT ?>/assets/images/dashboard.svg" alt="dashboard" /><span
                                 class="sidebar-titles">Dashboard</span></a>
                     </li>
 
                     <li>
-                        <a href="<?= ROOT ?>/src/roles/productionManager/html/customer_order/pending_custom_orders.html"><img src="<?= ROOT ?>/src/assets/order.svg" /><span
+                        <a href="<?= ROOT ?>/pending_custom_orders.php"><img src="<?= ROOT ?>/assets/images/order.svg" /><span
                                 class="sidebar-titles">Custom Orders</span></a>
                     </li>
                     <li>
-                        <a href="<?= ROOT ?>/src/roles/productionManager/html/recycled_polythene/recycled_polythene.html"><img src="<?= ROOT ?>/src/assets/recycle.svg" /><span
+                        <a href="<?= ROOT ?>/recycled_polythene.php"><img src="<?= ROOT ?>/assets/images/recycle.svg" /><span
                                 class="sidebar-titles">Recycled Polythene</span></a>
                     </li>
                     <li>
-                        <a href="<?= ROOT ?>/src/roles/productionManager/html/polythene_collection/schedule.html"><img
-                                src="<?= ROOT ?>/src/assets/collection.svg"
+                        <a href="<?= ROOT ?>/schedule.php"><img
+                                src="<?= ROOT ?>/assets/images/collection.svg"
                                 alt="site Performance" /><span class="sidebar-titles">Polythene Collection</span></a>
                     </li>
                     <li>
-                        <a href="<?= ROOT ?>/src/roles/productionManager/html/supply_requests/supply_request.html"><img src="<?= ROOT ?>/src/assets/supply.svg" alt="supply" /><span
+                        <a href="<?= ROOT ?>/supply_request.php"><img src="<?= ROOT ?>/assets/images/supply.svg" alt="supply" /><span
                                 class="sidebar-titles">Supply Requests</span></a>
                     </li>
                     <li>
-                        <a href="<?= ROOT ?>/src/roles/productionManager/html/pellets_requests/pellets_requests.html"><img src="<?= ROOT ?>/src/assets/order.svg" alt="supply" /><span
+                        <a href="<?= ROOT ?>/pellets_requests.php"><img src="<?= ROOT ?>/assets/images/order.svg" alt="supply" /><span
                                 class="sidebar-titles">Pellets Requests</span></a>
                     </li>
                 </ul>
@@ -58,13 +58,13 @@
     <div class="content">
         <header>
             <div class="logo">
-                <img src="<?= ROOT ?>/src/assets/Waste360.png" alt="logo" />
+                <img src="<?= ROOT ?>/assets/images/Waste360.png" alt="logo" />
                 <h1>Waste360</h1>
             </div>
             <h1 class="logo">DashBoard</h1>
             <nav class="nav">
                 <ul>
-                    <li><a href="#"><img src="<?= ROOT ?>/src/assets/notifications.svg"></a></li>
+                    <li><a href="#"><img src="<?= ROOT ?>/assets/images/notifications.svg"></a></li>
                     <li><a href="#">Profile</a></li>
                     <li><a href="#">Logout</a></li>
                 </ul>
@@ -120,8 +120,8 @@
         </div>
     </div>
 
-    <script src="<?= ROOT ?>/src/roles/productionManager/javaScript/sidebar.js"></script>
-    <script src="<?= ROOT ?>/src/roles/productionManager/javaScript/home.js"></script>
+    <script src="<?= ROOT ?>/assets/js/productionManager/sidebar.js"></script>
+    <script src="<?= ROOT ?>/assets/js/productionManager/home.js"></script>
     </main>
 </body>
 
