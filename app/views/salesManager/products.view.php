6d7ecbf81dde11dfd6ac3de1920aa5981022b052
Chamudi <129956853+chamudiupeka@users.noreply.github.com>
2025-04-22 19:18:15 +0530

    Fix title tag placement in products view

--- app/views/salesManager/products.view.php
+++ app/views/salesManager/products.view.php
@@ -4,10 +4,10 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Products List</title>
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/salesManager/common.css" />
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/salesManager/sidebar.css" />
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/salesManager/products.css" />
+    <title>Products List</title>
 </head>
 
 <body>
