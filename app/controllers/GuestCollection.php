47aebf62ee21ca07aa54778f9cae98e37a0d8171
Chamudi <129956853+chamudiupeka@users.noreply.github.com>
2025-04-29 01:48:53 +0530

    refactor: Clean up code formatting and improve readability in AcceptedGiveAwayRequest and GuestCollection controllers

--- app/controllers/GuestCollection.php
+++ app/controllers/GuestCollection.php
@@ -1,30 +1,44 @@
 <?php
 
-class GuestCollection {
+class GuestCollection
+{
 
   use Controller;
 
-  public function index() {
+  public function index()
+  {
     // $guestCollection = new GuestCollectionModel();
-    $this ->view('collectionAgent/guest_collection'); 
+    $this->view('collectionAgent/guest_collection');
   }
-  
-  public function save() {
-    if($_SERVER['REQUEST_METHOD'] == "POST") {
+
+  public function save()
+  {
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
       $guestCollection = new GuestCollectionModel();
-      
+
       $data = [
         'guest_name' => $_POST['guest_name'],
         'phone' => $_POST['phone'],
         'amount' => $_POST['amount'],
         'date' => date('Y-m-d') // Add today's date in YYYY-MM-DD format
       ];
-      
+
       $guestCollection->addCollection($data);
-      
+
+      //get carbonfootprint data
+      $carbonFootprintModel = new CarbonFootprintModel();
+      $carbonFootprintData = [
+        'customer_id' => null,
+        'name' => 'Collections',
+        'amount' => $_POST['amount'],
+        'carbon_footprint_type_id' => 3
+
+      ];
+      $carbonFootprintModel->addCarbonFootprint($carbonFootprintData);
+
       // Set success message in session
       $_SESSION['success_message'] = "Collection successfully added!";
-      
+
       redirect('GuestCollection');
     }
   }
